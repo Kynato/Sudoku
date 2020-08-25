d241816ae30d8f304740ce7d1ea282a01ba027d2
kynato <alanzbychu1998@gmail.com>
2020-08-25 22:25:29 +0200

    double grid added
    now the board is displayed in 3x3(3x3)
    
    it is just prettier haha

--- gui_fx.py
+++ gui_fx.py
@@ -18,17 +18,28 @@ class GUI:
         self.screen.fill(self.backgroundColor)
 
     def generateBackground(self):
-        boxSize = self.width/9
+        boxSpacing = self.width/9
         lineThickness = self.width*0.005
+        fatlineThickness = self.width*0.01
         offset = -(lineThickness/2)
-        barColor = (5,5,5)
+        fatOffset = -(fatlineThickness/2)
+        slimColor = (100,100,100)
+        fatColor = (200,200,200)
         # vBar - Vertical bar
         # hBar - Horizontal bar
         vBar = pygame.Rect(0, 0, 3, self.width)
         
+        # Slim lines
         for i in range(10):
             # Horizontal lines
-            pygame.draw.rect(self.screen, barColor, pygame.Rect(offset + i * boxSize, 0, lineThickness, self.width))
+            pygame.draw.rect(self.screen, slimColor, pygame.Rect(offset + i * boxSpacing, 0, lineThickness, self.width))
             # Vertical lines
-            pygame.draw.rect(self.screen, barColor, pygame.Rect(0, offset+ i * boxSize, self.width, lineThickness))
+            pygame.draw.rect(self.screen, slimColor, pygame.Rect(0, offset+ i * boxSpacing, self.width, lineThickness))
+
+        # Thicc lines
+        for i in range(4):
+            # Horizontal lines
+            pygame.draw.rect(self.screen, fatColor, pygame.Rect(fatOffset + i * (boxSpacing*3), 0, fatlineThickness, self.width))
+            # Vertical lines
+            pygame.draw.rect(self.screen, fatColor, pygame.Rect(0, fatOffset+ i * (boxSpacing*3), self.width, fatlineThickness))
 

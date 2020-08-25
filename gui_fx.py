ce0c17de02c26de05bda9909b8056167f9c84c53
Alan Hudela <alanzbychu1998@gmail.com>
2020-08-25 21:04:58 +0200

    checkerboard drawing beginnings

--- gui_fx.py
+++ gui_fx.py
@@ -14,3 +14,15 @@ class GUI:
         # Set the size of game window
         self.screen = pygame.display.set_mode((w, h))
         self.screen.fill((25,25,25)) # 2x parentheses bcs the arg= color
+
+    def generateBackground(self):
+        boxSize = self.width/9
+        barColor = (0,0,0)
+        # vBar - Vertical bar
+        # hBar - Horizontal bar
+        vBar = pygame.Rect(0, 0, 3, self.width)
+        
+        for i in range(10):
+            pygame.draw.rect(self.screen, barColor, pygame.Rect(i * boxSize, 0, self.width*0.005, self.width))
+            pygame.draw.rect(self.screen, barColor, pygame.Rect(0, i * boxSize, self.width, self.width*0.005))
+
--- gui_loop.py
+++ gui_loop.py
@@ -7,10 +7,11 @@ WINDOW_WIDTH = WINDOW_HEIGHT = 600
 
 # gui will be our graphics manager
 gui = GUI(WINDOW_WIDTH, WINDOW_HEIGHT)
+gui.generateBackground()
 
 while gui.state:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         gui.state = False
-        pygame.draw.rect(gui.screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+        #pygame.draw.rect(gui.screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
         pygame.display.flip()
